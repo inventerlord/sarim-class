48e0fbefb6a42a35d915ee326c0ad789a651080b
inventerlord <inventerlord@gmail.com>
2023-08-11 20:30:16 +0330

    lec001

---
+++ JSnNodejs/lec001/functions.js
@@ -0,0 +1,23 @@
+// normal non parametrize
+
+// function add() {
+//   console.log(10 + 2);
+// }
+// add();
+
+// parametrize
+// function add(a, b) {
+//   console.log(a + b);
+// }
+// add(10, 5);
+// defalt parametrize
+// function add(a = 0, b = 0) {
+//   console.log(a + b);
+// }
+// add();
+
+const add = (a, b) => {
+  console.log(a + b);
+};
+
+add();
---
+++ JSnNodejs/lec001/lec001.js
@@ -0,0 +1,14 @@
+let myname = "Sarim";
+let number = 786.23;
+let person = {
+  name: "sarim",
+  age: 21,
+  course: "Nodejs",
+};
+let members = ["book", 213];
+// null
+let n = null;
+// undefiened
+let un;
+console.log(members);
+// console.log(typeof number);
